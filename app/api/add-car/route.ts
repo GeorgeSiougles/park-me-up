edaaddfd0286d36ea46ddeddea9d8a5d41ee6039
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-01-27 17:53:34 +0200

    Backend validation with zod

--- app/api/add-car/route.ts
+++ app/api/add-car/route.ts
@@ -1,53 +1,25 @@
 import ParkedCar from "@/app/(models)/ParkedCar";
 import { NextResponse } from "next/server";
+
 import { z } from "zod";
+import { parkedCarValidator } from "@/lib/validators/ParkedCar";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const requestData = { ...body };
-    if (
-      requestData.carPlates.trim().length === 0 ||
-      requestData.carPlates.trim().length < 8
-    )
+    const result = parkedCarValidator.safeParse(requestData);
+    if (result.success) {
+      await ParkedCar.create(requestData);
       return NextResponse.json(
-        { message: "Car plates invalid" },
-        { status: 422 }
+        { message: "Car added to database" },
+        { status: 201 }
       );
-    if (!requestData.carPlates.includes("-"))
-      return NextResponse.json(
-        { message: "Car plates does not contain -" },
-        { status: 422 }
-      );
-    if (
-      requestData.carModel.trim().length === 0 ||
-      requestData.carModel.trim().length > 40
-    )
-      return NextResponse.json(
-        { message: "Car Model invalid" },
-        { status: 422 }
-      );
-    if (
-      requestData.ownerPhone.trim().length === 0 ||
-      requestData.ownerPhone.trim().length !== 10
-    )
-      return NextResponse.json(
-        { message: "Owner Phone invalid" },
-        { status: 422 }
-      );
-    if (
-      requestData.ownerPhone.trim().length === 0 ||
-      requestData.ownerPhone.trim().length !== 10
-    )
-      return NextResponse.json(
-        { message: "Owner Phone invalid" },
-        { status: 422 }
-      );
-    await ParkedCar.create(requestData);
-    return NextResponse.json(
-      { message: "Car added to database" },
-      { status: 201 }
+    }
+    const serverErrors = Object.fromEntries(
+      result.error?.issues.map((issue) => [issue.path[0], issue.message])
     );
+    return NextResponse.json({ errors: serverErrors });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return NextResponse.json(
