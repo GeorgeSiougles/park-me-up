6afd3ffaf9465de4b2fc830b22347cb8cdcf89ea
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-01-27 12:46:18 +0200

    Remove logs and add routes

--- app/api/add-car/route.ts
+++ app/api/add-car/route.ts
@@ -1,13 +1,10 @@
 import ParkedCar from "@/app/(models)/ParkedCar";
 import { NextResponse } from "next/server";
-// import { ParkedCar } from "@/app/types/ParkedCar";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const requestData = { ...body };
-    console.log("Request Data: ", requestData);
-    console.log(requestData);
     await ParkedCar.create(requestData);
     return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
   } catch (error) {
